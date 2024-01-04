b4dac7eca9cf65321caadecff6b4742bdb2435e0
Ellis velandia <eyis619@gmail.com>
2024-01-03 20:16:50 -0500

    main python

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("Hello world \nHello world 2")
+
+x = "ellis"
+y = "velandia"
+# z = x + y
+# print(f"{x} {y}")
+
+
+# ip = input("what us the target ip?")
+# print("scanning the target " + ip)
+# print("you are targeting "+input("What ip would you like target?"))
+
+a = "crisanto"
+b = 30
+c = "2"
+
+print(a + str(b))
+print(int(c) + b)
+
+fname = input("Whats your name? ")
+lname = input("what is your last name? ")
+
+print(f"your name is {fname} {lname}" )
