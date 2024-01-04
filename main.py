cb17cade474e6585dd5907883eb539181375b810
Ellis velandia <eyis619@gmail.com>
2024-01-03 20:54:53 -0500

    staments ifs

--- main.py
+++ main.py
@@ -24,12 +24,55 @@ c = "2"
 
 #create a greeting for your program
 
-greeting = "Hello world , welcome to cyber"
-username = input("what is your name? ")
+# greeting = "Hello world , welcome to cyber"
+# username = input("what is your name? ")
 #Ask for the user for the name of the pet.
-petname = input("what is the name of you pet? ")
+# petname = input("what is the name of you pet? ")
 #Ask for the name of the city you were born in 
-borncity = input("what is the name of the city were your born in? ")
+# borncity = input("what is the name of the city were your born in? ")
 #Combine the pet name with the word cyber as a new twitter handle and the add the city they are from.
 
-print(f"{greeting} Dear user, {username} your new twitter handle and bio {petname} from {borncity}")
+# print(f"{greeting} Dear user, {username} your new twitter handle and bio {petname} from {borncity}")
+
+#Data types
+
+# # strings
+# print(type("Hello world"))
+# # integer
+# print(type(5))
+# #float
+# print(type(4.4))
+# #Boolean
+# print(type(True))
+
+# fnum = input("what is the first number ")
+# snum = input("whta is the second number")
+
+# if fnum > snum:
+#     print("the first number is bigger")
+# elif snum > fnum:    
+#     print("the second number is larger!")    
+# else:
+#     print("the number are the same")
+
+score = input("what was your test score?")
+score = int(score)
+
+if score >= 98:
+    age = int(input("what is your age? "))
+    if age < 10:
+         print("Your grade is an A+")
+    else:
+         print("Your grade is an A") 
+elif score >= 80:
+    age = int(input("what is your age? "))
+    if age < 10:
+         print("Your grade is an B+")
+    else:  
+      print("Your grade is an B")
+elif score >= 78:
+      print("Your grade is an C")
+else:
+     print("Next time study more")         
+      
+
