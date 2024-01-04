9b797abe5e53b9a57b5fff083fa6cc79cc89a145
Ellis velandia <eyis619@gmail.com>
2024-01-03 20:33:16 -0500

    task 1

--- main.py
+++ main.py
@@ -14,10 +14,22 @@ a = "crisanto"
 b = 30
 c = "2"
 
-print(a + str(b))
-print(int(c) + b)
+# print(a + str(b))
+# print(int(c) + b)
 
-fname = input("Whats your name? ")
-lname = input("what is your last name? ")
+# fname = input("Whats your name? ")
+# lname = input("what is your last name? ")
 
-print(f"your name is {fname} {lname}" )
+# print(f"your name is {fname} {lname}" )
+
+#create a greeting for your program
+
+greeting = "Hello world , welcome to cyber"
+username = input("what is your name? ")
+#Ask for the user for the name of the pet.
+petname = input("what is the name of you pet? ")
+#Ask for the name of the city you were born in 
+borncity = input("what is the name of the city were your born in? ")
+#Combine the pet name with the word cyber as a new twitter handle and the add the city they are from.
+
+print(f"{greeting} Dear user, {username} your new twitter handle and bio {petname} from {borncity}")
